8501052c90709135c40fe49ef4dfb7cd2de68f61
Melvin Li <kkkwing15@163.com>
2023-01-28 11:56:41 +0800

    Update for Qt version 6.x
    Signed-off-by: Melvin Li <kkkwing15@163.com>

--- QSudokuForAndroid/main.cpp
+++ QSudokuForAndroid/main.cpp
@@ -1,10 +1,12 @@
-﻿#include "qsudokusolver.h"
+#include "qsudokusolver.h"
 #include <QApplication>
 #include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Unset);
+#elif (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)) && (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QApplication a(argc, argv);
